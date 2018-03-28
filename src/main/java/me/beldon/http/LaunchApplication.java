31559a5c5160981ddeacfbdb543e225d9f0a5392
Beldon <1047325205@qq.com>
2018-03-28 17:20:24 +0800

    init

---
+++ src/main/java/me/beldon/http/LaunchApplication.java
@@ -0,0 +1,15 @@
+package me.beldon.http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author beldon
+ */
+@SpringBootApplication
+public class LaunchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaunchApplication.class, args);
+    }
+}
