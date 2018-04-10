7ca20e88353feff32e695ea7614b9c14dcd81ad1
Beldon <1047325205@qq.com>
2018-04-10 09:19:16 +0800

    response demo

---
+++ src/main/java/me/beldon/http/constant/CommonConstant.java
@@ -0,0 +1,9 @@
+package me.beldon.http.constant;
+
+/**
+ * @author Beldon
+ * @create 2018-04-10 上午9:05
+ */
+public interface CommonConstant {
+    String PROTOCOL = "HTTP/1.1";
+}
--- src/main/java/me/beldon/http/server/ClientContext.java
+++ src/main/java/me/beldon/http/server/ClientContext.java
@@ -1,6 +1,7 @@
 package me.beldon.http.server;
 
 import lombok.extern.slf4j.Slf4j;
+import me.beldon.http.constant.CommonConstant;
 import me.beldon.http.context.CommonContextHolder;
 import me.beldon.http.context.Request;
 import me.beldon.http.handler.RequestHandler;
@@ -8,7 +9,9 @@ import me.beldon.http.handler.ResponseHandler;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
+import java.util.Date;
 
 /**
  * @author Beldon
@@ -37,12 +40,46 @@ public class ClientContext implements Runnable {
                 socketChannel.configureBlocking(false);
                 Request request = requestHandler.handle(socketChannel);
                 CommonContextHolder.RequestHolder.set(request);
+                response();
             }
         } catch (IOException e) {
             e.printStackTrace();
             log.error("请求异常", e);
         } finally {
             CommonContextHolder.RequestHolder.remove();
+            close();
+        }
+    }
+
+    private void response() {
+        String content = "hello world";
+        StringBuilder sb = new StringBuilder();
+        sb.append(CommonConstant.PROTOCOL).append(" 200 OK\r\n");
+        sb.append("Server: bel\r\n");
+        sb.append("Content-Type: text/html; charset=UTF-8\r\n");
+        sb.append("Date: " + new Date() + "\r\n");
+        sb.append("Content-Length: " + content.getBytes().length + "\r\n");
+
+        //响应内容
+        sb.append("\r\n");
+        sb.append(content);
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        buffer.put(sb.toString().getBytes());
+        buffer.flip();
+        try {
+            log.info("生成相应\r\n" + sb.toString());
+            socketChannel.write(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void close() {
+        try {
+            socketChannel.shutdownInput();
+            socketChannel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
