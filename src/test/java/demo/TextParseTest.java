227c8f7c04d21cc25015ede39030e1b104bfd11f
Beldon <1047325205@qq.com>
2018-07-23 18:38:39 +0800

    demo

--- src/test/java/demo/TextParseTest.java
+++ src/test/java/demo/TextParseTest.java
@@ -2,8 +2,10 @@ package demo;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
+import java.io.StringReader;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -11,9 +13,11 @@ import java.nio.channels.FileChannel;
  * @author Beldon
  * @create 2018-04-09 下午5:14
  */
+@SuppressWarnings("Duplicates")
 public class TextParseTest {
     private static final int LF = 10;
     private static final int CR = 13;
+
     @Test
     public void test() throws Exception {
         FileInputStream fileInputStream = new FileInputStream("html/demo.txt");
@@ -58,5 +62,53 @@ public class TextParseTest {
     }
 
 
+    private byte crlf13 = (byte) 13; //碶 ?br/>
+    private byte crlf10 = (byte) 10;  //碶 ?/FONT>
+
+    private StringBuffer request = new StringBuffer();  //用于保存所有内容
+
+    @Test
+    public void read() throws Exception {
+        String targetFile = "html/demo.txt";
+
+        FileInputStream fis = new FileInputStream(targetFile);
+
+        byte[] crlf = new byte[1];
+        int crlfNum = 0;   //已经连接的回车换行数 crlfNum=4为头部结束
+        while (fis.read(crlf) != -1)   //读取头部
+        {
+            if (crlf[0] == crlf10) {
+                crlfNum++;
+            } else {
+                crlfNum = 0;
+
+            }
+            request.append(new String(crlf, 0, 1));  //byte数组相加
+            if (crlfNum == 2) {
+                break;
+            }
+        }
+        System.out.println(request.toString());
+        System.out.println("----");
+
+
+        StringReader reader = new StringReader(request.toString());
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        System.out.println(bufferedReader.readLine());
+
+    }
+
+
+    @Test
+    public void handleRequest() throws Exception {
+        String targetFile = "html/temp.txt";
+
+        FileInputStream fis = new FileInputStream(targetFile);
+
+        byte[] data = new byte[1];
+        while (fis.read(data) != -1) {
+            System.out.println(data[0]);
+        }
+    }
 
 }
